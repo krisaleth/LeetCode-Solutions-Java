5076cc3e3be3ec3d7710f23efbdb31a0d1df6f16
KimTam <kimtam486@gmail.com>
2026-02-07 22:22:55 +0700

    Time: 1 ms (60.73%), Space: 49.2 MB (8.44%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -4,26 +4,25 @@ class Solution {
         int[] maxRightArr = new int[height.length];
         int maxLeft = Integer.MIN_VALUE;
         int maxRight = Integer.MIN_VALUE;
-        int waterBlock = 0;
-
+        int water = 0;
         for (int i = 0; i < height.length; i++) {
-            if (height[i] > maxLeft) {
+            if (maxLeft < height[i]) {
                 maxLeft = height[i];
             }
             maxLeftArr[i] = maxLeft;
         }
 
         for (int i = height.length - 1; i >= 0; i--) {
-            if (height[i] > maxRight) {
+            if (maxRight < height[i]) {
                 maxRight = height[i];
             }
             maxRightArr[i] = maxRight;
         }
 
         for (int i = 0; i < height.length; i++) {
-            waterBlock = waterBlock + (Math.min(maxLeftArr[i], maxRightArr[i]) - height[i]);
+            water += Math.min(maxLeftArr[i], maxRightArr[i]) - height[i];
         }
 
-        return waterBlock;
+        return water;
     }
 }

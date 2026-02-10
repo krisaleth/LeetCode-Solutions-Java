9351444e8789218d862fd7ee198e41f909299257
KimTam <kimtam486@gmail.com>
2026-02-10 12:57:06 +0700

    0141-LINKED-LIST-CYCLE Stats: Time: 0 ms (100%), Space: 46.6 MB (85.49%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -15,14 +15,12 @@ public class Solution {
         ListNode slow = head;
 
         while (fast != null && fast.next != null) {
-            fast = fast.next.next;
             slow = slow.next;
-
+            fast = fast.next.next;
             if (fast == slow) {
                 return true;
             }
         }
-
         return false;
     }
 }

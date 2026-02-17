0b4edd318ed8208ed528615f5561f4adb1dc5d20
Krisaleth <kimtam486@gmail.com>
2026-02-17 20:09:52 +0700

    0150-EVALUATE-REVERSE-POLISH-NOTATION Stats: Time: 6 ms (90%), Space: 45.5 MB (31.44%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
@@ -3,26 +3,26 @@ class Solution {
         Stack<Integer> tokenStack = new Stack<>();
 
         for (int i = 0; i < tokens.length; i++) {
-            if (!"+-*/".contains(tokens[i])) {
-                tokenStack.push(Integer.parseInt(tokens[i]));
-            }
-            else {
-                int num2 = tokenStack.pop();
-                int num1 = tokenStack.pop();
-
                 if ("+".equals(tokens[i])) {
-                    tokenStack.push(num1 + num2);
+                    tokenStack.push(tokenStack.pop() + tokenStack.pop());
                 }
                 else if ("-".equals(tokens[i])) {
+                    int num2 = tokenStack.pop();
+                    int num1 = tokenStack.pop();
                     tokenStack.push(num1 - num2);
                 }
                 else if ("*".equals(tokens[i])) {
-                    tokenStack.push(num1 * num2);
+                    tokenStack.push(tokenStack.pop() * tokenStack.pop());
                 }
-                else {
+                else if ("/".equals(tokens[i])) {
+                    int num2 = tokenStack.pop();
+                    int num1 = tokenStack.pop();
                     tokenStack.push(num1 / num2);
                 }
-             }
+            else {
+                tokenStack.push(Integer.parseInt(tokens[i]));
+            }
+           
         }
 
         return tokenStack.pop();

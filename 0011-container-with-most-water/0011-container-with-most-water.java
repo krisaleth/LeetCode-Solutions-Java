fab87a4d31982c97c045a6ec6392e60659a8a71c
Krisaleth <kimtam486@gmail.com>
2026-02-23 20:52:23 +0700

    0011-CONTAINER-WITH-MOST-WATER Stats: Time: 5 ms (81.29%), Space: 77.5 MB (48.02%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -1,15 +1,15 @@
 class Solution {
-    public int maxArea(int[] height) {
+    public int maxArea(int[] heights) {
+        int max = Integer.MIN_VALUE;
+
         int left = 0;
-        int right = height.length - 1;
-        int maxWater = 0;
+        int right = heights.length - 1;
 
         while (left < right) {
-            int water = Math.min(height[left], height[right]) * (right - left);
-            if (water > maxWater) {
-                maxWater = water;
-            }
-            if (height[left] > height[right]) {
+            int res = Math.min(heights[left], heights[right]) * (right - left);
+            max = Math.max(max, res);
+
+            if (heights[left] >= heights[right]) {
                 right--;
             }
             else {
@@ -17,6 +17,6 @@ class Solution {
             }
         }
 
-        return maxWater;
+        return max;
     }
 }

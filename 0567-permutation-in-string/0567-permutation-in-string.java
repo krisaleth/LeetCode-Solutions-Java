311b3c5999523192f8da62892b44bfdce638f6c1
Krisaleth <kimtam486@gmail.com>
2026-03-02 22:57:08 +0700

    0567-PERMUTATION-IN-STRING Stats: Time: 9 ms (45.69%), Space: 43.9 MB (94.41%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,38 +1,38 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        if (s1.length() > s2.length()) {
-            return false;
-        }
-
-        Map<Character, Integer> s1Map = new HashMap<>();
-        Map<Character, Integer> s2Map = new HashMap<>();
-
-        for (char s : s1.toCharArray()) {
-            s1Map.put(s, s1Map.getOrDefault(s, 0) + 1);
-        }
+        if (s1.length() > s2.length()) return false;
+        int[] sChar = new int[26];
+        int count = 0;
 
         for (int i = 0; i < s1.length(); i++) {
-            s2Map.put(s2.charAt(i), s2Map.getOrDefault(s2.charAt(i), 0) + 1);
+            sChar[s1.charAt(i) - 'a']++;
+            sChar[s2.charAt(i) - 'a']--;
         }
 
-        int head = 0;
-        int tail = s1.length() - 1;
+        for (int s : sChar) {
+            if (s == 0) {
+                count++;
+            }
+        }
 
-        while (tail < s2.length()) {
-            if (s1Map.equals(s2Map)) {
+        if (count == 26) {
+            return true;
+        }
+
+        for (int i = s1.length(); i < s2.length(); i++) { 
+            int right = s2.charAt(i) - 'a';
+            if (sChar[right] == 0) count--; 
+            sChar[right]--;                 
+            if (sChar[right] == 0) count++; 
+
+            int left = s2.charAt(i - s1.length()) - 'a';
+            if (sChar[left] == 0) count--; 
+            sChar[left]++;                 
+            if (sChar[left] == 0) count++;
+            
+            if (count == 26) {
                 return true;
             }
-            if (s2Map.get(s2.charAt(head)) > 1) {
-                s2Map.put(s2.charAt(head), s2Map.get(s2.charAt(head)) - 1);
-            }
-            else {
-                s2Map.remove(s2.charAt(head));
-            }
-            if (tail + 1 < s2.length()) {
-                s2Map.put(s2.charAt(tail + 1), s2Map.getOrDefault(s2.charAt(tail + 1), 0) + 1);
-            }
-            head++;
-            tail++;
         }
 
         return false;
